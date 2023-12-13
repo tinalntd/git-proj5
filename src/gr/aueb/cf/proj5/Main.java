835f51bf18b5bff9a8a4dc669aa60ced26723fb1
Tina Leontidou <tinaleontidou@gmail.com>
2023-12-13 12:19:44 +0200

    Main updated

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -3,7 +3,7 @@ package gr.aueb.cf.proj5;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello CF5!");
+        System.out.println("Hello Coding Factory!!!!");
     }
 }
 
